aaf5877057a7ab5e97611b783d24bbcd1fed0423
Freezman13 <58755601+Freezman13@users.noreply.github.com>
2020-01-22 14:29:47 -0600

    Readability changes

--- application.js
+++ application.js
@@ -1,13 +1,16 @@
-$(function(){
-
-    L.Control.LiveMouseCoordinatesControl = L.Control.extend({
-        options: {
+$(function()
+{
+    L.Control.LiveMouseCoordinatesControl = L.Control.extend(
+	{
+        options: 
+		{
             position: 'bottomright',
             labelTemplate: "Coordinates: {0}",
             labelFormatter: undefined
         },
 
-        onAdd: function(map){
+        onAdd: function(map)
+		{
             this._map = map;
 
             var className = 'leaflet-control-mapcodes',
@@ -26,48 +29,55 @@ $(function(){
             return container;
         },
 
-        onRemove: function(map){
+        onRemove: function(map)
+		{
             map.off("mousemove", this._update, this);
         },
 
-        _update: function(evt){
+        _update: function(evt)
+		{
             var pos = evt.latlng;
             var opts = this.options;
 
-            if (pos) {
+            if (pos) 
+			{
                 pos = pos.wrap();
                 var mapcode = this.x_y_map_code(pos);
                 this._label.innerHTML = this._createMapcodeLabel(mapcode);
             }
         },
+		
 		// x y coordinates
-		x_y_map_code: function(point){
+		x_y_map_code: function(point)
+		{
             var x = point.lng;
 			var y = point.lat;
             return x.toFixed(1) + ', ' + y.toFixed(1);
         },
 
-        _createMapcodeLabel: function(mapcode){
+        _createMapcodeLabel: function(mapcode)
+		{
             var opts = this.options, l;
-            if (opts.labelFormatter) {
+            if (opts.labelFormatter) 
+			{
                 l = opts.labelFormatter(mapcode);
-            } else {
+            } 
+			else 
+			{
                 l = L.Util.template(opts.labelTemplate, {0: mapcode});
             }
             return l;
         }
     });
 
-    L.control.liveCoordinates = function(opts) {
+    L.control.liveCoordinates = function(opts) 
+	{
         return new L.Control.LiveMouseCoordinatesControl(opts);
     };
-
-	// variables defined with json links from the published google doc(s)
-    var googleSheetJsonUrl_1 = 'https://spreadsheets.google.com/feeds/list/1PIISVofJmBh0dNr4OkCzfepFKLSL2i5CUrGEdMhUnuA/2/public/values?alt=json'
-	var googleSheetJsonUrl_2 = 'https://spreadsheets.google.com/feeds/list/1PIISVofJmBh0dNr4OkCzfepFKLSL2i5CUrGEdMhUnuA/3/public/values?alt=json'
 	
 	// unused but the application expects tiles
-	var officialMapLayer = L.tileLayer("tiles/{z}/{x}/{y}.png", {
+	var officialMapLayer = L.tileLayer("tiles/{z}/{x}/{y}.png", 
+	{
     maxZoom: 6,
     minZoom: 1,
     bounds: L.latLngBounds([0,0],[-256,256]),
@@ -77,7 +87,8 @@ $(function(){
 	// map layer
     var the_map = L.imageOverlay('AAmap.png', [[0,0], [-259,256.5]]); 
 
-    var map = L.map("map", {
+    var map = L.map("map", 
+	{
         crs: L.CRS.Simple,
         layers: [officialMapLayer, the_map],
         maxZoom: 6,
@@ -85,45 +96,51 @@ $(function(){
     }).setView([-128, 128], 2);
 
 	//filters
-	
 	var Clover = new L.LayerGroup();
 	var Iris = new L.LayerGroup();
 
-	//filters
-	var baseLayers = {
+	//defining the base layer for the filter box - the world map.
+	var baseLayers = 
+	{
 	"ArcheAge Map": the_map
 	};
 	
-	var overlays = {
+	// defining overlays - used as resource filters
+	var overlays = 
+	{
 	"Clovers" : Clover,
 	"Iris" : Iris
 	};
 	
+	// Adds base layer and overlays to the map
     L.control.layers(baseLayers, overlays).addTo(map);
-    
-
     L.control.liveCoordinates({ position: 'bottomright' }).addTo(map);
 
-// defining the icon class
-var LeafIcon = L.Icon.extend({             
-    options: {
-        iconSize:     [30, 30],
-        iconAnchor:   [15, 15],
-        popupAnchor:  [-3, -76]
-    }
-});
+	// Defining the icon class
+	var Icon_Class = L.Icon.extend(
+	{             
+    options: 
+		{
+			iconSize:     [30, 30],
+			iconAnchor:   [15, 15],
+			popupAnchor:  [-3, -76]
+		}
+	});
 
-// defining specific icons
-var clover_icon = new LeafIcon({iconUrl: 'images/Clover.png'}),
-    iris_icon = new LeafIcon({iconUrl: 'images/Iris.png'}),
-    orangeIcon = new LeafIcon({iconUrl: 'leaf-orange.png'});
+	// Defining specific icons
+	var clover_icon = new Icon_Class({iconUrl: 'images/Clover.png'}),
+		iris_icon = new Icon_Class({iconUrl: 'images/Iris.png'});
+	
+	// Stores the data from the published JSON
+	var googleSheetJsonUrl_1 = 'https://spreadsheets.google.com/feeds/list/1PIISVofJmBh0dNr4OkCzfepFKLSL2i5CUrGEdMhUnuA/'+2+'/public/values?alt=json'
+	var googleSheetJsonUrl_2 = 'https://spreadsheets.google.com/feeds/list/1PIISVofJmBh0dNr4OkCzfepFKLSL2i5CUrGEdMhUnuA/3/public/values?alt=json'
 
-	//Markers from sheet 1 placed on the map
-    $.ajax({
-        url: googleSheetJsonUrl_1
-    }).done(function(data) {
+	// Markers from sheet 1 placed on the map
+    $.ajax({url: googleSheetJsonUrl_1}).done(function(data)
+	{
         console.log('google json', data);
-        data.feed.entry.forEach(function(entry){
+        data.feed.entry.forEach(function(entry)
+		{
 			var marker_1 = L.marker([entry['gsx$y-axis']['$t'], entry['gsx$x-axis']['$t']], {icon: clover_icon});
 			// on click popup sample code
          /* var markerPopupHtml = "<strong>Type: </strong>" + entry['gsx$group']['$t'];
@@ -137,18 +154,13 @@ var clover_icon = new LeafIcon({iconUrl: 'images/Clover.png'}),
     });
 
 	//sheet 2
-	    $.ajax({
-        url: googleSheetJsonUrl_2
-    }).done(function(data) {
-        console.log('google json', data);
-        data.feed.entry.forEach(function(entry){
+	$.ajax({url: googleSheetJsonUrl_2}).done(function(data)
+	{
+		console.log('google json', data);
+		data.feed.entry.forEach(function(entry)
+		{
             var marker_2 = L.marker([entry['gsx$y-axis']['$t'], entry['gsx$x-axis']['$t']], {icon: iris_icon});
             marker_2.addTo(Iris)
         })
     });
-	var cities = L.layerGroup([marker_1]);
 });
-
-// TODO
-// https://github.com/CliffCloud/Leaflet.EasyButton
-// https://github.com/lvoogdt/Leaflet.awesome-markers
