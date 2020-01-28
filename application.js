5c2649c80fc8cea11fd689851feba4d0ba607284
Freezman13 <58755601+Freezman13@users.noreply.github.com>
2020-01-27 20:04:04 -0600

    User input now case insensitive, adjusted comments and variable names

--- application.js
+++ application.js
@@ -85,9 +85,9 @@ $(function()
     });0
 	
 	// Values to change when adding things
-	var N_of_sheets = 1+3;		// +1 when adding a sheet   // an extra 1 becuase google sheets start counting at 1
-	var N_of_filters = 4;		// +1 when adding a filter box
-	var N_of_resources = 4;		// +1 when adding a resource
+	var N_of_sheets = 1+3;		// +1 when adding a sheet  		 // an extra 1 becuase google sheets start counting at 1
+	var N_of_filters = 1+5;		// +1 when adding a filter box	 // an extra 1 because filter 0 corresponds to icon 0 which is a placeholders
+	var N_of_resources = 5;		// +1 when adding a resource
 	
 	// map layer
     var the_map = L.imageOverlay('AAmap.png', [[0,0], [-259,256.5]]); 
@@ -108,14 +108,13 @@ $(function()
 	};
 
 	var h;
-	for(h = 0; h < N_of_filters; h++)
+	for(h = 1; h < N_of_filters; h++)		// h = 1 and not 0 becuase icon 0 is for placeholders and doens't need a filter
 	{
 		eval("var filter_" + h + " = new L.LayerGroup();");
 	}	   // var filter_[h] = new L.LayerGroup(); // where [h] increments
 		
 	// turns the filters on by default
 	/*
-    map.addLayer(filter_0) 
 	map.addLayer(filter_1)	     
 	map.addLayer(filter_2)	 
 	map.addLayer(filter_3)	
@@ -124,10 +123,11 @@ $(function()
 	// defining overlays - used as resource filters - needs a new entry every sheet
 	var overlays = 
 	{
-		"Clovers" : filter_0,
-		"Iris" : filter_1,
-		"Thistle" : filter_3,
-		"Mushroom" : filter_2
+		"Clovers" : filter_1,
+		"Iris" : filter_2,
+		"Mushroom" : filter_3,
+		"Thistle" : filter_4,
+		"Placeholder_1" : filter_5
 
 	}	// "NAME_OF_FILTER_HERE" : filter_			
 	// copy / paste / change the name that will display / add the next number to filter_ / add a comma to the previous one
@@ -147,53 +147,57 @@ $(function()
 		}
 	});
 	
-	// Stores the data from the published JSON
+	// Stores the data from the published doc's JSON
 	var j;
 	for(j = 1; j < N_of_sheets; j++)
 	{										
 		eval('var googleSheetJsonUrl_' + j + '= ' + "'https://spreadsheets.google.com/feeds/list/1wtX_-TxHgM62Zk2MBFRzEY-sy03RtUxiSNe7_ykdfB4/'" + '+(j)+' + "'/public/values?alt=json'");
 	}      // var googleSheetJsonUrl_[j] = 'https://spreadsheets.google.com/feeds/list/1PIISVofJmBh0dNr4OkCzfepFKLSL2i5CUrGEdMhUnuA/[j]/public/values?alt=json' // where [j] increments
 																																								  
-	// Defining specific icons
+	// Defining icon variables and linking them to icon images
 	var i;
 	for(i = 0; i < N_of_resources; i++)
 	{
 		eval('var icon_' + i + " = new Icon_Class({iconUrl: 'images/'+ i +'.png'});");
 	}      // var icon_[i] = new Icon_Class({iconUrl: 'images/[i].png'}); // where [i] increments
 
-	// Markers from a sheet placed on the map - needs a new entry every sheet
+	// Markers from a sheet placed on the map - needs a new getter every sheet and a new switch every resource
 	// discord bot sheet
-	var k = 1; // used to itterate the url call of the getter
+	var k = 1; // used to itterate the url call of the getters
     $.get({url: eval("googleSheetJsonUrl_"+ k)}).then(function(data)
 	{
         data.feed.entry.forEach(function(entry)
 		{
 			var resource_name = entry['gsx$resource']['$t']; // stores the resource name from the sheet
-			var resource_icon;
-			var icon_layer;
+			var marker_icon;								 // used to pass the icon that a marker will be assigned to
+			var filter_layer;								 // used to pass the filter to which a marker will be assigned to
 			
-			switch(resource_name)	 		// Switch for icon selection and layer placement for filtering
+			switch(resource_name.toLowerCase())	 		// Switch for icon selection and layer placement for filtering
 			{
-				case "Clover":		 		// reads the resource name from the sheet		
-				resource_icon = "icon_0";   // assigns the first icon
-				icon_layer = "filter_0";	// assigns it to the first layer
+				case "clover":		 		// reads the resource name from the sheet		
+				marker_icon = "icon_1";   // assigns icon 1 to the marker
+				filter_layer = "filter_1";	// assigns the marker to filter 1
 				break;
-				case "Iris":
-				resource_icon = "icon_1";
-				icon_layer = "filter_1";
+				case "iris":
+				marker_icon = "icon_2";
+				filter_layer = "filter_2";
 				break;
-				case "Mushroom":
-				resource_icon = "icon_2";
-				icon_layer = "filter_2";
+				case "mushroom":
+				marker_icon = "icon_3";
+				filter_layer = "filter_3";
 				break;
-				case "Thistle":
-				resource_icon = "icon_3";
-				icon_layer = "filter_3";
+				case "thistle":
+				marker_icon = "icon_4";
+				filter_layer = "filter_4";
+				break;
+				case "placeholder_1":
+				marker_icon = "icon_0";
+				filter_layer = "filter_5";
 				break;
 			}	
 			// adds the markers to the coordinates taken from the sheet and assigns an icon
-			var marker = L.marker([entry['gsx$y-axis']['$t'], entry['gsx$x-axis']['$t']], {icon: eval(resource_icon)});
-			marker.addTo(eval(icon_layer))
+			var marker = L.marker([entry['gsx$y-axis']['$t'], entry['gsx$x-axis']['$t']], {icon: eval(marker_icon)});
+			marker.addTo(eval(filter_layer))
 			
 			// adds a popup if the sheet has something written in the popup cell
 			var markerPopupHtml;
@@ -206,37 +210,41 @@ $(function()
 	})
 	k++;
 	
-	// google form sheet
+	// google form sheet in case the bot is down
 	$.get({url: eval("googleSheetJsonUrl_"+ k)}).then(function(data)
 	{
         data.feed.entry.forEach(function(entry)
 		{
 			var resource_name = entry['gsx$resource']['$t'];
-			var resource_icon;
-			var icon_layer;
+			var marker_icon;
+			var filter_layer;
 			
-			switch(resource_name)
+			switch(resource_name.toLowerCase())
 			{
-				case "Clover":
-				resource_icon = "icon_0";
-				icon_layer = "filter_0";
+				case "clover":		
+				marker_icon = "icon_1"; 
+				filter_layer = "filter_1";
 				break;
-				case "Iris":
-				resource_icon = "icon_1";
-				icon_layer = "filter_1";
+				case "iris":
+				marker_icon = "icon_2";
+				filter_layer = "filter_2";
 				break;
-				case "Mushroom":
-				resource_icon = "icon_2";
-				icon_layer = "filter_2";
+				case "mushroom":
+				marker_icon = "icon_3";
+				filter_layer = "filter_3";
 				break;
-				case "Thistle":
-				resource_icon = "icon_3";
-				icon_layer = "filter_3";
+				case "thistle":
+				marker_icon = "icon_4";
+				filter_layer = "filter_4";
+				break;
+				case "placeholder_1":
+				marker_icon = "icon_0";
+				filter_layer = "filter_5";
 				break;
 			}	
 				
-			var marker = L.marker([entry['gsx$y-axis']['$t'], entry['gsx$x-axis']['$t']], {icon: eval(resource_icon)});
-			marker.addTo(eval(icon_layer))
+			var marker = L.marker([entry['gsx$y-axis']['$t'], entry['gsx$x-axis']['$t']], {icon: eval(marker_icon)});
+			marker.addTo(eval(filter_layer))
 			
 			var markerPopupHtml;
             if (entry['gsx$popup']['$t'] != "")
@@ -254,31 +262,35 @@ $(function()
         data.feed.entry.forEach(function(entry)
 		{
 			var resource_name = entry['gsx$resource']['$t'];
-			var resource_icon;
-			var icon_layer;
+			var marker_icon;
+			var filter_layer;
 			
-			switch(resource_name)
+			switch(resource_name.toLowerCase())
 			{
-				case "Clover":
-				resource_icon = "icon_0";
-				icon_layer = "filter_0";
+				case "clover":		
+				marker_icon = "icon_1"; 
+				filter_layer = "filter_1";
 				break;
-				case "Iris":
-				resource_icon = "icon_1";
-				icon_layer = "filter_1";
+				case "iris":
+				marker_icon = "icon_2";
+				filter_layer = "filter_2";
 				break;
-				case "Mushroom":
-				resource_icon = "icon_2";
-				icon_layer = "filter_2";
+				case "mushroom":
+				marker_icon = "icon_3";
+				filter_layer = "filter_3";
 				break;
-				case "Thistle":
-				resource_icon = "icon_3";
-				icon_layer = "filter_3";
+				case "thistle":
+				marker_icon = "icon_4";
+				filter_layer = "filter_4";
+				break;
+				case "Placeholder_1":
+				marker_icon = "icon_0";
+				filter_layer = "filter_5";
 				break;
 			}	
 				
-			var marker = L.marker([entry['gsx$y-axis']['$t'], entry['gsx$x-axis']['$t']], {icon: eval(resource_icon)});
-			marker.addTo(eval(icon_layer))
+			var marker = L.marker([entry['gsx$y-axis']['$t'], entry['gsx$x-axis']['$t']], {icon: eval(marker_icon)});
+			marker.addTo(eval(filter_layer))
 			
 			var markerPopupHtml;
             if (entry['gsx$popup']['$t'] != "")
